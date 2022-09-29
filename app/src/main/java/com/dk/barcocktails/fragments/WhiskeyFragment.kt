f9ea84286b5931b6519ec480c669310ffc275f00
dk <omnipresent9@yandex.ru>
2022-09-29 22:30:51 +0300

    добавил вкладку Виски

--- app/src/main/java/com/dk/barcocktails/fragments/WhiskeyFragment.kt
+++ app/src/main/java/com/dk/barcocktails/fragments/WhiskeyFragment.kt
@@ -5,10 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import com.dk.barcocktails.R
+import com.dk.barcocktails.adapters.CocktailAdapter
 import com.dk.barcocktails.databinding.FragmentWhiskeyBinding
+import com.dk.barcocktails.domain.FillRecyclerView
 
 class WhiskeyFragment : Fragment() {
+    private val imList = intArrayOf(
+    R.drawable.bushmills_3_1,
+    R.drawable.bushmils_cola,
+    R.drawable.old_fashioned,
+    R.drawable.manhattan,
+    R.drawable.penicillin,
+    R.drawable.fire_breeze,
+    R.drawable.apple_jack,
+    R.drawable.hello_honey,
+    R.drawable.bushmills_sour,
+    R.drawable.red_hot_chili_sour,
+    R.drawable.mint_julep,
+    R.drawable.favorite_singer,
+    R.drawable.irish_coffee,
+    R.drawable.forever_young
+    )
+
     lateinit var whiskeyBinding: FragmentWhiskeyBinding
+    lateinit var rcW: RecyclerView
+    lateinit var adapter: CocktailAdapter
+    val fillRecyclerView = FillRecyclerView()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,6 +40,18 @@ class WhiskeyFragment : Fragment() {
     ): View? {
         whiskeyBinding = FragmentWhiskeyBinding.inflate(inflater, container, false)
         return whiskeyBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val list = fillRecyclerView.fillArray(resources.getTextArray(R.array.whiskeyCocktails),
+            imList)
+
+        adapter = CocktailAdapter(list)
+
+        rcW = whiskeyBinding.rcW
+        rcW.adapter = adapter
     }
 
 }
